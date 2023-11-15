b6431fadd130e7b4e68deb7baf7d23d45e40d895
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2023-11-15 21:20:51 +0700

    commit

---
+++ Bai8_Nguyen114_P2/Bai1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Bai1
+{
+    internal class Program
+    {
+        delegate double PhepTinh(double x,double y);
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Chon phep tinh:");
+                Console.WriteLine("1. Tong");
+                Console.WriteLine("2. Hieu");
+                Console.WriteLine("3. Tich");
+                Console.WriteLine("4. Thuong");
+                Console.WriteLine("5. Thoat");
+                Console.Write("Nhap lua chon cua ban: ");
+                string choice = Console.ReadLine();
+
+                Console.Write("Nhap so thu nhat: ");
+                double firstNumber = double.Parse(Console.ReadLine());
+                Console.Write("Nhap so thu hai: ");
+                double secondNumber = double.Parse(Console.ReadLine());
+
+                PhepTinh phepTinh = null;
+
+                switch (choice)
+                {
+                    case "1":
+                        phepTinh = TingTong;
+                        break;
+                    case "2":
+                        phepTinh = TinhHieu;
+                        break;
+                    case "3":
+                        phepTinh = TinhTich;
+                        break;
+                    case "4":
+                        phepTinh = TinhThuong;
+                        break;
+                    case "5":
+                        Console.WriteLine("Da thoat chuong trinh");
+                        return;
+                    default:
+                        Console.WriteLine("Lua chon khong phu hop!!!");
+                        break;
+                }
+
+                if(phepTinh != null)
+                {
+                    double result = phepTinh(firstNumber, secondNumber);
+                    Console.WriteLine("Ket qua: " + result);
+                }
+            }
+        }
+
+        static double TingTong(double a, double b)
+        {
+            return a + b;
+        }
+
+        static double TinhHieu(double a, double b)
+        {
+            return a - b;
+        }
+
+        static double TinhTich(double a, double b)
+        {
+            return a * b;
+        }
+
+        static double TinhThuong(double a, double b)
+        {
+            if (b != 0)
+            {
+                return a / b;
+            }
+            else
+            {
+                Console.WriteLine("Khong the chia cho 0.");
+                return 0;
+            }
+        }
+    }
+}
---
+++ Bai8_Nguyen114_P2/Bai2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bai2
+{
+    delegate void NhapVaHienThiDanhSachSinhVienDelegate(List<SinhVien> danhsach);
+
+    class SinhVien
+    {
+        private string masinhvien;
+        public string MaSinhVien
+        {
+            get { return masinhvien; }
+            set { masinhvien = value; }
+        }
+
+        private string hoten;
+        public string HoTen
+        {
+            get { return hoten; }
+            set { hoten = value; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<SinhVien> danhsachSinhVien = new List<SinhVien>();
+            NhapVaHienThiDanhSachSinhVienDelegate nhapDelegate = NhapDanhSachSinhVien;
+            NhapVaHienThiDanhSachSinhVienDelegate hienthiDelegate = HienThiDanhSachSinhVien;
+
+            // Use the combined delegate
+            NhapVaHienThiDanhSachSinhVienDelegate nhapVaHienThiDelegate;
+            nhapVaHienThiDelegate = nhapDelegate + hienthiDelegate;
+
+            nhapVaHienThiDelegate(danhsachSinhVien);
+        }
+
+        static void HienThiDanhSachSinhVien(List<SinhVien> list)
+        {
+            Console.WriteLine("Danh sach sinh vien:");
+            foreach(SinhVien sv in list)
+            {
+                Console.WriteLine("Ma sinh vien: {0}, Ho ten sinh vien: {1}", sv.MaSinhVien, sv.HoTen);
+            }
+        }
+
+        static void NhapDanhSachSinhVien(List<SinhVien> list)
+        {
+            Console.Write("Nhap so luong sinh vien: ");
+            int soluong = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < soluong; i++)
+            {
+                SinhVien sinhVien = new SinhVien();
+                Console.WriteLine("Nhap thong tin sinh vien thu {0}: ", i+1);
+                Console.Write("Nhap ma sinh vien: ");
+                sinhVien.MaSinhVien = Console.ReadLine();
+                Console.Write("Nhap ho ten sinh vien: ");
+                sinhVien.HoTen = Console.ReadLine();
+
+                list.Add(sinhVien);
+            }
+        }
+    }
+}
