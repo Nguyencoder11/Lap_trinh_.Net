fce606a7cb5b5ec6c737461bed6efd3c2d90241e
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2023-11-19 15:03:08 +0700

    commit

--- BAI9_Nguyen114_WPFproject/ChonSach/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ BAI9_Nguyen114_WPFproject/ChonSach/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace ChonSach {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ChonSach;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ChonSach;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- BAI9_Nguyen114_WPFproject/TinhTienDien/MainWindow.xaml.cs
+++ BAI9_Nguyen114_WPFproject/TinhTienDien/MainWindow.xaml.cs
@@ -24,5 +24,42 @@ namespace TinhTienDien
         {
             InitializeComponent();
         }
+
+        double soKwTieuThu, tongTien;
+        private void btnTinh_Click(object sender, RoutedEventArgs e)
+        {
+            double chiSoCu, chiSoMoi, soKwTrongDinhMuc, soKwVuotDinhMuc = 0;
+            chiSoCu = double.Parse(txtChiSoCu.Text);
+            chiSoMoi = double.Parse(txtChiSoMoi.Text);
+            soKwTieuThu = chiSoMoi - chiSoCu;
+            if(soKwTieuThu <= 50)
+            {
+                soKwTrongDinhMuc = soKwTieuThu;
+                tongTien = soKwTieuThu * 500;
+            }
+            else
+            {
+                soKwTrongDinhMuc = 50;
+                soKwVuotDinhMuc = soKwTieuThu - 50;
+                tongTien = 25000 + soKwVuotDinhMuc * 1000;
+            }
+            txtSoKwTieuThu.Text = soKwTieuThu.ToString();
+            txtTongTienTra.Text = tongTien.ToString("NO");
+            txtSoKwTrongDinhMuc.Text = soKwTrongDinhMuc.ToString();
+            txtSoKwVuotDinhMuc.Text = soKwVuotDinhMuc.ToString();
+        }
+
+        private void btnIn_Click(object sender, RoutedEventArgs e)
+        {
+            lstThongTin.Items.Clear();
+            lstThongTin.Items.Add(cboHoTen.Text);
+            lstThongTin.Items.Add("Số kw tiêu thụ: " + soKwTieuThu);
+            lstThongTin.Items.Add("Tổng tiền: " + tongTien.ToString("NO"));
+        }
+
+        private void btnThoat_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
