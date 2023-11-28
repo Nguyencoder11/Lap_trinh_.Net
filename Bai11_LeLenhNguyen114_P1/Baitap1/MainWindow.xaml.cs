91dc6d4ac387fcd2c615f077565009ffe391b6e5
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2023-11-28 13:17:39 +0700

    commit

--- Bai11_LeLenhNguyen114_P1/Baitap1/MainWindow.xaml.cs
+++ Bai11_LeLenhNguyen114_P1/Baitap1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using BaiTap1.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -30,6 +31,11 @@ namespace BaiTap1
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            HienThi();
+        }
+
+        private void HienThi()
+        {
             var query = from sp in db.SanPhams
                         join lsp in db.LoaiSanPhams on sp.MaLoai equals lsp.MaLoai
                         select new
@@ -41,6 +47,16 @@ namespace BaiTap1
                             lsp.TenLoai
                         };
             dtgDanhSachSanPham.ItemsSource = query.ToList();
+        }
+
+        private void Clear_TextBox()
+        {
+            // Dua cac textbox ve trang thai chua nhap
+            txtMaSp.Text = string.Empty;
+            txtTenSp.Text = string.Empty;
+            txtSoLuong.Text = string.Empty;
+            txtDonGia.Text = string.Empty;
+            txtMaLoai.Text = string.Empty;
         }
 
         private void dtgSanPham_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -67,18 +83,110 @@ namespace BaiTap1
 
         private void btnThem_Click(object sender, RoutedEventArgs e)
         {
-            SanPham spMoi = new SanPham();
+            string masp = txtMaSp.Text.Trim();
+            // Kiem tra ma da ton tai trong CSDL chua
+            var existingID = db.SanPhams.FirstOrDefault(sp => sp.MaSp == masp);
+            if (existingID != null)
+            {
+                MessageBox.Show("Ma san pham da ton tai. Hay chon ma san pham khac.", "Thong bao", MessageBoxButton.OK, MessageBoxImage.Warning);
+                txtMaSp.Focus();
+                return;
+            }
 
+            SanPham spMoi = new SanPham();
+            spMoi.MaSp = txtMaSp.Text;
+            spMoi.TenSp = txtTenSp.Text;
+            spMoi.SoLuong = int.Parse(txtSoLuong.Text);
+            spMoi.DonGia = int.Parse(txtDonGia.Text);
+            spMoi.MaLoai = txtMaLoai.Text;
+
+            // Them vao csdl
+            db.SanPhams.Add(spMoi);
+
+            // luu thay doi csdl
+            db.SaveChanges();
+
+            // Clear cac textbox
+            Clear_TextBox();
+
+            // hien thi danh sach sau khi them
+            HienThi();
         }
 
         private void btnSua_Click(object sender, RoutedEventArgs e)
         {
+            string masp = txtMaSp.Text;
+            // Kiem tra neu ma san pham khong duoc nhap
+            if (string.IsNullOrEmpty(masp))
+            {
+                MessageBox.Show("Vui long nhap ma san pham can sua.", "Thong bao", MessageBoxButton.OK, MessageBoxImage.Warning);
+                txtMaSp.Focus();
+                return;
+            }
 
+            // Tim san pham trong csdl
+            var sanpham = db.SanPhams.FirstOrDefault(sp => sp.MaSp == masp);
+            
+            // Neu khong tim thay dua ra thong bao
+            if (sanpham == null) {
+                MessageBox.Show("Khong tim thay san pham co ma " + masp + ".", "Thong bao", MessageBoxButton.OK, MessageBoxImage.Warning);
+                txtMaSp.Focus();
+                return;
+            }
+
+            // nguoclai tien hanh cap nhat
+            sanpham.TenSp = txtTenSp.Text;
+            sanpham.SoLuong = int.Parse(txtSoLuong.Text);
+            sanpham.DonGia = int.Parse(txtDonGia.Text);
+            sanpham.MaLoai = txtMaLoai.Text;
+
+            // luu thay doi csdl
+            db.SaveChanges();
+
+            // Clear cac textbox
+            Clear_TextBox();
+
+            // Hien thi danh sach sau khi cap nhat
+            HienThi();
         }
 
         private void btnXoa_Click(object sender, RoutedEventArgs e)
         {
+            string masp = txtMaSp.Text;
+            // Kiem tra neu masp khong duoc nhap
+            if (string.IsNullOrEmpty(masp)) {
+                MessageBox.Show("Vui long nhap ma san pham can xoa.", "Thong bao", MessageBoxButton.OK, MessageBoxImage.Warning);
+                txtMaSp.Focus();
+                return;
+            }
 
+            // Tim san pham can xoa trong csdl
+            var sanpham = db.SanPhams.FirstOrDefault(sp => sp.MaSp == masp);
+
+            // Neu khong tim thay dua ra thong bao
+            if (sanpham == null)
+            {
+                MessageBox.Show("Khong tim thay san pham co ma " + masp + ".", "Thong bao", MessageBoxButton.OK, MessageBoxImage.Warning);
+                txtMaSp.Focus();
+                return;
+            }
+
+            // Hien thi hop thoai san pham xoa
+            var confirmMessage = MessageBox.Show("Ban co muon xoa san pham co ma " + masp + "?", "Xac nhan xoa", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            // Xoa san pham khi chon Yes
+            if(confirmMessage == MessageBoxResult.Yes)
+            {
+                // Xoa san pham khoi csdl
+                db.SanPhams.Remove(sanpham);
+                db.SaveChanges();
+
+                // Clear cac textbox
+                Clear_TextBox();
+
+                // hien thi danh sach sau khi xoa
+                HienThi();
+            }
         }
 
         private void btnThoat_Click(object sender, RoutedEventArgs e)
